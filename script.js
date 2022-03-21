b59a2b5714a205ab1023da1fe2cbd3b189e3ccdb
Vlad Sava <savavlad82@gmail.com>
2022-03-21 20:48:58 +0200

    Fix width and height of each grid box
    Fixed the size of each grid box so that regardless of the grid size,
    each grid box has equal width and height, while the container itself
    is the exact same size.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const gridContainer = document.querySelector('.grid');
 
-for(let i = 1; i <= 16; i++)
+for(let i = 1; i <= 16; i++){
+    let row = document.createElement('div');
+    row.classList.add('grid-row');
+    gridContainer.appendChild(row);
     for(let j = 1; j <= 16; j++){
         let element = document.createElement('div');
         element.classList.add('grid-box');
-        gridContainer.appendChild(element);
+        row.appendChild(element);
     }
+}
