4c2b311d007847f4bdb758feac1de8f0cbd550de
Vlad Sava <savavlad82@gmail.com>
2022-03-22 17:09:04 +0200

    Add way of changing the size of the grid
    Added buttons to change the size of the grid:
    16x16; 32x32; 64x64;

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const gridContainer = document.querySelector('.grid');
 const clearBtn = document.querySelector('#clear');
 
+GenerateGrid(16);
+
+
+clearBtn.addEventListener('click', Clear);
 function Clear(){
     const filled = document.querySelectorAll('.clicked');
     filled.forEach(btn => {
@@ -8,18 +12,23 @@ function Clear(){
     })
 }
 
-clearBtn.addEventListener('click', Clear);
-
-for(let i = 1; i <= 16; i++){
-    let row = document.createElement('div');
-    row.classList.add('grid-row');
-    gridContainer.appendChild(row);
-    for(let j = 1; j <= 16; j++){
-        let element = document.createElement('div');
-        element.classList.add('grid-box');
-        row.appendChild(element);
-    }
+function GenerateGrid(x){
+    gridContainer.innerHTML = '';
+    for(let i = 1; i <= x; i++){
+        let row = document.createElement('div');
+        row.classList.add('grid-row');
+        gridContainer.appendChild(row);
+        for(let j = 1; j <= x; j++){
+            let element = document.createElement('div');
+            element.classList.add('grid-box');
+            row.appendChild(element);
+        }
+    }    
 }
+
+document.getElementById('option-one').addEventListener('click', () => GenerateGrid(16));
+document.getElementById('option-two').addEventListener('click', () => GenerateGrid(32));
+document.getElementById('option-three').addEventListener('click', () => GenerateGrid(64));
 
 const gridBoxes = document.querySelectorAll('.grid-box');
 
